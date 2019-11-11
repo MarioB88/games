11970dba2c33243249265e149ae6dc5eb44b5559
MARIO BERLANGA SERRANO <Mario.Berlanga@alu.uclm.es>
2019-11-11 14:25:44 +0100

    Funtional snake game. Random snake not done

--- Snake.py
+++ Snake.py
@@ -53,8 +53,8 @@ class snake(object):
 
     def move(self):
         for event in pygame.event.get():
-            #if event.type == pygame.QUIT:
-            #    pygame.quit()
+            if event.type == pygame.QUIT:
+                pygame.quit()
             keys = pygame.key.get_pressed()
 
             for key in keys:
@@ -70,12 +70,12 @@ class snake(object):
 
                 elif keys[pygame.K_UP]:
                     self.dx = 0
-                    self.dy = 1
+                    self.dy = -1
                     self.turns[self.head.pos[:]] = [self.dx, self.dy]
 
                 elif keys[pygame.K_DOWN]:
                     self.dx = 0
-                    self.dy = -1
+                    self.dy = 1
                     self.turns[self.head.pos[:]] = [self.dx, self.dy]
 
         for i, c in enumerate(self.body):
@@ -86,17 +86,36 @@ class snake(object):
                 if i == len(self.body)-1:
                     self.turns.pop(p)
             else:
-                if c.dirnx == -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
-                elif c.dirnx == 1 and c.pos[0] >= c.rows-1: c.pos = (0,c.pos[1])
-                elif c.dirny == 1 and c.pos[1] >= c.rows-1: c.pos = (c.pos[0], 0)
-                elif c.dirny == -1 and c.pos[1] <= 0: c.pos = (c.pos[0],c.rows-1)
+                if c.dx == -1 and c.pos[0] <= 0: c.pos = (rows-1, c.pos[1])
+                elif c.dx == 1 and c.pos[0] >= rows-1: c.pos = (0,c.pos[1])
+                elif c.dy == 1 and c.pos[1] >= rows-1: c.pos = (c.pos[0], 0)
+                elif c.dy == -1 and c.pos[1] <= 0: c.pos = (c.pos[0],rows-1)
                 else: c.move(c.dx, c.dy)
 
     def reset(self, pos):
-        pass 
+        self.head = cube(pos)
+        self.body = []
+        self.body.append(self.head)
+        self.turns = {}
+        self.dx = 0
+        self.dy = 1
+
 
     def addCube(self):
-        pass
+        tail = self.body[-1]
+        dx, dy = tail.dx, tail.dy
+        if dx == 1 and dy == 0:
+            self.body.append(cube((tail.pos[0]-1,tail.pos[1])))
+        elif dx == -1 and dy == 0:
+            self.body.append(cube((tail.pos[0]+1,tail.pos[1])))
+        elif dx == 0 and dy == 1:
+            self.body.append(cube((tail.pos[0],tail.pos[1]-1)))
+        elif dx == 0 and dy == -1:
+            self.body.append(cube((tail.pos[0],tail.pos[1]+1)))
+        
+        self.body[-1].dx = dx
+        self.body[-1].dy = dy
+        
 
     def draw(self, surface):
         for i, c in enumerate(self.body):
@@ -122,15 +141,52 @@ def redrawWindow(surface):
     global rows, width
     surface.fill((0,0,0))
     s.draw(surface)
+    snack.draw(surface)
+    obst.draw(surface)
     drawGrid(width, rows, surface)
     pygame.display.update()
 
+def randomSnack(rows, snake):
+    positions = snake.body
+    while True:
+        x = random.randrange(rows)
+        y = random.randrange(rows)
+        if len(list(filter(lambda z:z.pos == (x,y), positions))) > 0:
+            continue
+        else:
+            break
+    return (x, y)
+
+def randomObstacle(rows, snake, snackPos):
+    positions = snake.body
+    while True:
+        x=random.randrange(rows)
+        y=random.randrange(rows)
+
+        if (len(list(filter(lambda z:z.pos == (x,y), positions))) > 0) or (snackPos == (x,y)):
+            continue
+        else:
+            break
+    return (x,y)
+
+def messageBox(subject, content):
+    root = tk.Tk()
+    root.attributes("-topmost", True)
+    root.withdraw()
+    messagebox.showinfo(subject, content)
+    try:
+        root.destroy()
+    except:
+        pass
+
 def main():
-    global width, rows, s
+    global width, rows, s, snack, obst
     width = 500
     rows = 20
     window = pygame.display.set_mode((width, width))
     s = snake((255, 0, 0), (10,10))
+    snack = cube(randomSnack(rows, s), color = (0, 255, 0))
+    obst = cube(randomObstacle(rows, s, snack.pos), color = (0,0, 255))
 
     clock = pygame.time.Clock()
 
@@ -139,5 +195,14 @@ def main():
         clock.tick(10)              # Nos asegura que nuestro juego no se va a ejecutar a mas de 10 frames por segundo (la serpiente se movera diez bloques en un segundo)
 
         redrawWindow(window)
+        s.move()
+        if (s.body[0].pos == snack.pos):
+            s.addCube()
+            snack = cube(randomSnack(rows, s), color = (0, 255, 0))
+        
+        if (s.body[0].pos in list(map(lambda z:z.pos, s.body[1:]))) or (s.body[0].pos == obst.pos):
+            print("Score: ", len(s.body))
+            messageBox("You lost!", "Play Again...?")
+            s.reset((10,10))
 
 main()
